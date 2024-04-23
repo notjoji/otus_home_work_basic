99dfbb8d9649d5f99c83000080bc68869bc680e8
Aleksey Baydin <aleksey.baydin@ural.rt.ru>
2024-04-23 11:13:44 +0500

    HW2 is completed

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -1,14 +1,15 @@
-package init
+package main
 
 import (
-	"github.com/fixme_my_friend/hw02_fix_app/printer"
-	"github.com/fixme_my_friend/hw02_fix_app/reader"
-	"github.com/fixme_my_friend/hw02_fix_app/types"
 	"fmt"
+
+	"github.com/notjoji/otus_home_work_basic/hw02_fix_app/printer"
+	"github.com/notjoji/otus_home_work_basic/hw02_fix_app/reader"
+	"github.com/notjoji/otus_home_work_basic/hw02_fix_app/types"
 )
 
-func init() {
-	var path string = "data.json"
+func main() {
+	path := "data.json"
 
 	fmt.Printf("Enter data file path: ")
 	fmt.Scanln(&path)
@@ -18,12 +19,12 @@ func init() {
 
 	if len(path) == 0 {
 		path = "data.json"
-	} else {
 	}
 
-	staff, err = reader.ReadJSON(path, -1)
-
-	fmt.Print(err)
+	staff, err = reader.ReadJSON(path)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+	}
 
 	printer.PrintStaff(staff)
 }
