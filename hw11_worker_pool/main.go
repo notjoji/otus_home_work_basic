cb21dba73b1735e25100aa9b2deaeed1b0999be5
Aleksey Baydin <aleksey.baydin@ural.rt.ru>
2024-07-02 20:34:49 +0500

    HW11 completed

--- hw11_worker_pool/main.go
+++ hw11_worker_pool/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+func InitWorkerPool(size int) int {
+	var x int
+	wg := sync.WaitGroup{}
+	mu := sync.Mutex{}
+
+	for i := 0; i < size; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			mu.Lock()
+			x++
+			mu.Unlock()
+			fmt.Println("x:", x)
+		}()
+	}
+
+	wg.Wait()
+	return x
+}
+
 func main() {
-	// Place your code here.
+	fmt.Println("result:", InitWorkerPool(1000))
 }
---
+++ hw11_worker_pool/worker_pool_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInitWorkerPool(t *testing.T) {
+	testCases := []struct {
+		name string
+		size int
+		want int
+	}{
+		{
+			"simple_test",
+			1000,
+			1000,
+		},
+		{
+			"lots_of_workers_test",
+			1000000,
+			1000000,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			assert.Equal(t, tc.want, InitWorkerPool(tc.size))
+		})
+	}
+}
