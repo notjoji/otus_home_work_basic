cbc55baa11beb5e95d755ac844f17b9583173c1d
Aleksey Baydin <aleksey.baydin@ural.rt.ru>
2024-08-25 18:09:50 +0500

    HW9 rewrote MarshalJSON and UnmarshalJSON functions

--- hw09_serialize/main.go
+++ hw09_serialize/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/notjoji/otus_home_work_basic/hw09_serialize/book"
 	"log"
+
+	"github.com/notjoji/otus_home_work_basic/hw09_serialize/book"
 )
 
 type Book struct {
-	Id     int32   `json:"id,omitempty"`
+	ID     int32   `json:"id,omitempty"`
 	Title  string  `json:"title,omitempty"`
 	Author string  `json:"author,omitempty"`
 	Year   int32   `json:"year,omitempty"`
@@ -16,13 +17,14 @@ type Book struct {
 	Rate   float32 `json:"rate,omitempty"`
 }
 
-func (b Book) String() string {
-	return fmt.Sprintf("id: %d title: %s author: %s year: %d size: %d rate: %f", b.Id, b.Title, b.Author, b.Year, b.Size, b.Rate)
+func (b *Book) String() string {
+	return fmt.Sprintf("id: %d title: %s author: %s year: %d size: %d rate: %f", b.ID, b.Title, b.Author, b.Year,
+		b.Size, b.Rate)
 }
 
-func Map(b *book.Book) Book {
+func MapFromProto(b *book.Book) Book {
 	return Book{
-		Id:     b.Id,
+		ID:     b.Id,
 		Title:  b.Title,
 		Author: b.Author,
 		Year:   b.Year,
@@ -31,38 +33,37 @@ func Map(b *book.Book) Book {
 	}
 }
 
-func (b Book) MarshalJSON() ([]byte, error) {
-	return json.Marshal(b)
+func MapToProto(b Book) *book.Book {
+	return &book.Book{
+		Id:     b.ID,
+		Title:  b.Title,
+		Author: b.Author,
+		Year:   b.Year,
+		Size:   b.Size,
+		Rate:   b.Rate,
+	}
 }
 
-func (b Book) UnmarshalJSON(data []byte) error {
-	return json.Unmarshal(data, &b)
+func (b *Book) MarshalJSON() ([]byte, error) {
+	protoBook := MapToProto(*b)
+	return json.Marshal(&protoBook)
+}
+
+func (b *Book) UnmarshalJSON(data []byte) error {
+	var b1 book.Book
+	err := json.Unmarshal(data, &b1)
+	if err != nil {
+		return err
+	}
+	*b = MapFromProto(&b1)
+	return nil
 }
 
 func MarshalToJSON(arr []Book) ([]byte, error) {
-	//data := make([]byte, len(arr))
 	return json.Marshal(arr)
-	//for i, elem := range arr {
-	//	marshalled, err := elem.MarshalJSON()
-	//	if err != nil {
-	//		return nil, err
-	//	}
-	//	data[i] = marshalled
-	//}
-	//return data, nil
 }
 
 func UnmarshalFromJSON(data []byte) ([]*Book, error) {
-	//res := make([]*MyBook, len(data))
-	//for i, elem := range data {
-	//	b := MyBook{}
-	//	err := b.UnmarshalJSON(elem)
-	//	if err != nil {
-	//		return nil, err
-	//	}
-	//	res[i] = &b
-	//}
-	//return res, nil
 	res := make([]*Book, 0)
 	err := json.Unmarshal(data, &res)
 	return res, err
@@ -70,7 +71,7 @@ func UnmarshalFromJSON(data []byte) ([]*Book, error) {
 
 func main() {
 	b1 := Book{
-		Id:     1,
+		ID:     1,
 		Title:  "A Game Of Thrones (A Song of Ice and Fire)",
 		Author: "George RR Martin",
 		Year:   1997,
@@ -78,7 +79,7 @@ func main() {
 		Rate:   9.1,
 	}
 	b2 := Book{
-		Id:     2,
+		ID:     2,
 		Title:  "The Lord of the Rings",
 		Author: "John R.R. Tolkien",
 		Year:   1954,
